6a6bcd78943bdf5239a4b33babc4133e32b109d0
Saonela <777skumbre@gmail.com>
2016-02-18 23:15:48 +0200

    Log/reg completed

--- src/RTER/UserBundle/Form/Type/RegistrationType.php
+++ src/RTER/UserBundle/Form/Type/RegistrationType.php
@@ -2,27 +2,24 @@
 namespace  RTER\UserBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Util\LegacyFormHelper;
 
 class RegistrationType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
-//        $builder
-//            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
-//            ->remove('username')
-//            ->remove('email')
-//            ->add('name', 'text', array('label' => 'Vardas'))
-//            ->add('surname', 'text', array('label' => 'Pavardė'))
-//            ->add('plainPassword', 'repeated', array(
-//                'type' => 'password',
-//                'options' => array('translation_domain' => 'FOSUserBundle'),
-//                'first_options' => array('label' => 'form.password'),
-//                'second_options' => array('label' => 'form.password_confirmation'),
-//                'invalid_message' => 'fos_user.password.mismatch',
-//            ))
-//        ;
+        $builder
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
     }
 
 
